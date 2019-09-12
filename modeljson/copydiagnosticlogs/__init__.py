18c8c6c86b2eac936c00748d22a40d1f5c78cdd0
Rene Bremer <rebremer@microsoft.com>
2019-09-12 19:51:44 +0200

    add SPN authentication possibility in copydiagnostics logs function

--- modeljson/copydiagnosticlogs/__init__.py
+++ modeljson/copydiagnosticlogs/__init__.py
@@ -1,19 +1,33 @@
 import logging
+import os
+#import uuid
+from adal import AuthenticationContext
+from azure.storage.blob import (
+    AppendBlobService,
+    BlockBlobService,
+    BlobPermissions,
+    ContainerPermissions
+)
+from azure.storage.common import (
+    TokenCredential
+)
+import azure.functions as func
+from msrestazure.azure_active_directory import MSIAuthentication
+from jsonschema import validate
+import collections
 import json
 
-import azure.functions as func
-from azure.storage.blob import BlobService
 from msrestazure.azure_active_directory import MSIAuthentication
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     logging.info(req.get_body())
-    #inputreq = json.loads(req.get_body())
+    inputreq = json.loads(req.get_body())
 
-    from_storage_account = "testedlstorgen" #inputreq['fromStorage']
-    to_storage_account = "testedlloggingstor" #inputreq['toStorage']
-    to_storage_account_container = "diagnosticslogs" #inputreq['toConainer']
+    from_storage_account = inputreq['fromStorage'] # "testedlstorgen"
+    to_storage_account =  inputreq['toStorage'] # "testedlloggingstor"
+    to_storage_account_container = inputreq['toContainer'] # "diagnosticslogs"
 
     # In case you cannot use MSI (e.g. when you run this script on a local pc, you can create credentials using an SPN)
     #RESOURCE = "https://storage.azure.com/"
@@ -31,8 +45,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     # MSI
     credentials = MSIAuthentication(resource='https://storage.azure.com/')
-    blob_service_from = BlobService(from_storage_account, token_credential=credentials)
-    blob_service_to = BlobService(account_name=to_storage_account, token_credential=credentials)
+    blob_service_from = BlockBlobService(from_storage_account, token_credential=credentials)
+    blob_service_to = BlockBlobService(account_name=to_storage_account, token_credential=credentials)
 
     generator = blob_service_from.list_blobs("$logs")
     for blob in generator:
